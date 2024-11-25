aacc8952079792a9f1ecb33b1389a4071db58ca0
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-25 23:34:00 +0100

    added taskcards to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { TaskCard } from "@/features/tasks/task-card";
+
 export default function Home() {
   return (
     <>
       <div>Welcome to the Task Manager!</div>
+      <TaskCard />
     </>
   );
 }
