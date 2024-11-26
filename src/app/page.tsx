d7e43e56d3ce110091243ab74c3fec9ba6ffaa23
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-26 17:52:53 +0100

    added empty div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import { TaskCard } from "@/features/tasks/task-card";
 export default function Home() {
   return (
     <>
-      <div>Welcome to the Task Manager!</div>
-      <TaskCard />
+      <div>
+        <div>Welcome to the Task Manager!</div>
+        <TaskCard />
+      </div>
     </>
   );
 }
