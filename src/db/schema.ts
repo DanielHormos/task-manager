a2c9c48b28824a52860bc54ddd92995fd561b1b4
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-26 18:21:58 +0100

    fixed spelling on schema to db

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,5 +3,5 @@ import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 export const tasksTable = pgTable("tasks", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   task: varchar({ length: 255 }).notNull(),
-  frequecy: varchar({ length: 255 }).notNull(),
+  frequency: varchar({ length: 255 }).notNull(),
 });
