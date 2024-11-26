a08e605bac645b7e2058e5c9ba4d1c0c7b4a9774
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-26 17:53:51 +0100

    have db as parameter inside create service

--- src/features/tasks/service.ts
+++ src/features/tasks/service.ts
@@ -1,9 +1,15 @@
-import { Repository } from "./repository";
+import { TaskInsert } from "@/db/types";
+import { Db } from "@/index";
+import { createRepository } from "./repository";
 
-export function createService(repository: Repository) {
+export function createService(db: Db) {
+  const repository = createRepository(db);
   return {
     async fetchTasks() {
       return await repository.fetchTasks();
+    },
+    async postTask(task: TaskInsert) {
+      return await repository.postTask(task);
     },
   };
 }
