bc3e6969b1f00b509230aee96d28b63b964ee283
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-26 20:57:06 +0100

    added startday into schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,5 +3,6 @@ import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 export const tasksTable = pgTable("tasks", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   task: varchar({ length: 255 }).notNull(),
-  frequency: varchar({ length: 255 }).notNull(),
+  startDay: varchar({ length: 10 }).notNull(),
+  frequency: varchar({ length: 8 }).notNull(),
 });
