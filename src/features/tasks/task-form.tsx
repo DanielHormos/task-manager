862370466e704534094556f2d536bf7f3d477cb5
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-26 20:56:29 +0100

    added startday for task do be done

--- src/features/tasks/task-form.tsx
+++ src/features/tasks/task-form.tsx
@@ -34,6 +34,22 @@ export function TaskForm() {
         <option value="weekly">Weekly</option>
         <option value="monthly">Monthly</option>
       </select>
+      <label htmlFor="start-day" className="block mt-2 text-lg font-medium">
+        Start day
+      </label>
+      <select
+        name="start-day"
+        id="start-day"
+        className="w-full p-2 mb-4 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      >
+        <option value="Monday">Monday</option>
+        <option value="Tuesday">Tuesday</option>
+        <option value="Wednesday">Wednesday</option>
+        <option value="Thursday">Thursday</option>
+        <option value="Friday">Friday</option>
+        <option value="Saturday">Saturday</option>
+        <option value="Sunday">Sunday</option>
+      </select>
 
       <button
         type="submit"
