95ba2c37deab932746a6860efecaded18f7a51a4
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-26 20:56:43 +0100

    added startday to action

--- src/features/tasks/action.ts
+++ src/features/tasks/action.ts
@@ -1,11 +1,12 @@
 "use server";
 import { TaskInsert } from "@/db/types";
-import { featureService } from "./feature";
+import { featureService } from "./instance";
 
 export async function postTaskAction(data: FormData) {
   const task = data.get("task") as string;
   const frequency = data.get("frequency") as string;
-  const taskToPost: TaskInsert = { task, frequency };
+  const startDay = data.get("start-day") as string;
+  const taskToPost: TaskInsert = { task, frequency, startDay };
   await featureService.postTask(taskToPost);
 }
 
