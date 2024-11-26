4018ee7877596f8c88a71f01d4990dfe55322d01
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-26 17:53:23 +0100

    moved out submit form function to action file

--- src/features/tasks/task-form.tsx
+++ src/features/tasks/task-form.tsx
@@ -1,7 +1,12 @@
+import { postTaskAction } from "./action";
+
 export function TaskForm() {
   return (
-    <form className="flex flex-col items-center  p-6 max-w-md border border-black rounded-md">
-      <h2 className="text-2xl font-bold ">Add Task</h2>
+    <form
+      action={postTaskAction}
+      className="flex flex-col items-center p-6 max-w-md border border-black rounded-md"
+    >
+      <h2 className="text-2xl font-bold">Add Task</h2>
 
       <label
         htmlFor="task"
@@ -12,9 +17,11 @@ export function TaskForm() {
       <input
         id="task"
         type="text"
+        name={"task"}
         placeholder="Enter task"
         className="w-full p-2 mb-4 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
+
       <label htmlFor="frequency" className="block mt-2 text-lg font-medium">
         Frequency
       </label>
@@ -27,9 +34,10 @@ export function TaskForm() {
         <option value="weekly">Weekly</option>
         <option value="monthly">Monthly</option>
       </select>
+
       <button
         type="submit"
-        className="w-full bg-blue-500 text-white mt-4 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 border  rounded-md p-2"
+        className="w-full bg-blue-500 text-white mt-4 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 border rounded-md p-2"
       >
         Add
       </button>
