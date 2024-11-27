832a9500a3972b748fef96a139d8ff8d4a2b146c
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-27 23:07:13 +0100

    added completed and completer to schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,4 +4,6 @@ export const tasksTable = pgTable("tasks", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   task: varchar({ length: 20 }).notNull(),
   frequency: varchar({ length: 8 }).notNull(),
+  completed: varchar({ length: 5 }).notNull(),
+  completer: varchar({ length: 20 }),
 });
