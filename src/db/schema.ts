8d2b666fff6cca139b4349a01efc307f3fa8349a
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-27 21:39:32 +0100

    removed startday attribute from schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,7 +2,6 @@ import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const tasksTable = pgTable("tasks", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  task: varchar({ length: 255 }).notNull(),
-  startDay: varchar({ length: 10 }).notNull(),
+  task: varchar({ length: 20 }).notNull(),
   frequency: varchar({ length: 8 }).notNull(),
 });
