e471841374f37e5f8a3a4a3fe956b7763d274037
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-27 23:07:49 +0100

    added completeTask function in action

--- src/features/tasks/action.ts
+++ src/features/tasks/action.ts
@@ -5,8 +5,18 @@ import { featureService } from "./instance";
 export async function postTaskAction(data: FormData) {
   const task = data.get("task") as string;
   const frequency = data.get("frequency") as string;
-  const taskToPost: TaskInsert = { task, frequency };
+  const taskToPost: TaskInsert = {
+    task,
+    frequency,
+    completed: "false",
+    completer: "",
+  };
   await featureService.postTask(taskToPost);
+}
+
+export async function completeTaskAction(data: FormData, id: number) {
+  const completer = data.get("completer") as string;
+  await featureService.completeTask(id, completer);
 }
 
 export async function fetchTasksAction() {
