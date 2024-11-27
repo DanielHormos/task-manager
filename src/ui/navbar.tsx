3941dd0524872c39fc0f5123caaf3aa769d2ff81
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-27 21:38:16 +0100

    fixed navbar

--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -2,15 +2,20 @@ import Link from "next/link";
 
 const navItems = [
   { name: "Home", href: "/" },
-  { name: "Add Task", href: "/add-task" },
+  { name: "Add-Task", href: "/add-task" },
 ];
 
 export function Navbar() {
   return (
-    <ul className="flex space-x-4">
+    <ul className="flex gap-4 bg-gray-800 text-white py-4 justify-center items-center">
       {navItems.map((item) => (
         <li key={item.href}>
-          <Link href={item.href}>{item.name}</Link>
+          <Link
+            href={item.href}
+            className="text-white hover:text-gray-300 rounded-md text-lg font-medium"
+          >
+            {item.name}
+          </Link>
         </li>
       ))}
     </ul>
