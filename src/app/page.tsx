ef4b402fd9bb749e391a7663718615e6c47e5720
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-27 21:44:03 +0100

    added sections for daily, weekly and monthly tasks that need to be done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
+import { fetchTasksAction } from "@/features/tasks/action";
 import { TaskCard } from "@/features/tasks/task-card";
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await fetchTasksAction();
+
+  const dailyTasks = tasks.filter((task) => task.frequency === "daily");
+  const weeklyTasks = tasks.filter((task) => task.frequency === "weekly");
+  const monthlyTasks = tasks.filter((task) => task.frequency === "monthly");
+
   return (
-    <>
-      <div>
-        <div>Welcome to the Task Manager!</div>
-        <TaskCard />
+    <div className=" flex flex-col  w-full h-screen pt-8 items-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-center mb-16">Tasks</h1>
+      <div className="flex gap-20">
+        <section>
+          <h2 className="text-2xl font-bold text-center mb-4">Daily</h2>
+          <TaskCard tasks={dailyTasks} />
+        </section>
+        <section>
+          <h2 className="text-2xl font-bold text-center mb-4">Weekly</h2>
+          <TaskCard tasks={weeklyTasks} />
+        </section>
+        <section>
+          <h2 className="text-2xl font-bold text-center mb-4">Monthly</h2>
+          <TaskCard tasks={monthlyTasks} />
+        </section>
       </div>
-    </>
+    </div>
   );
 }
