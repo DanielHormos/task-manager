988052a0650cb5a526664324b7538337bd8989ee
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-28 14:15:25 +0100

    removed unnessesary code

--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -3,31 +3,21 @@ import Link from "next/link";
 const navItems = [
   { name: "Home", href: "/" },
   { name: "Add Task", href: "/add-task" },
-  { name: "Team", href: "/" },
+  { name: "Team", href: "/team" },
 ];
 
 export function Navbar() {
   return (
     <div className="navbar bg-base-100 shadow-md">
-      <div className="navbar-start">
-        <Link href="/" className="btn btn-ghost normal-case text-xl">
-          Task Manager
-        </Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          {navItems.map((item) => (
-            <li key={item.href}>
-              <Link href={item.href} className="btn btn-ghost">
-                {item.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <button className="btn btn-primary">Login</button>
-      </div>
+      <ul className="menu menu-horizontal px-1">
+        {navItems.map((item) => (
+          <li key={item.href}>
+            <Link href={item.href} className="btn btn-ghost">
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
