dfdfaca4ce47242bed5f9d30368d4b972a449a7b
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-28 22:19:12 +0100

    added resettaskbutton inside homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { fetchTasksAction } from "@/features/tasks/action";
 import { TaskCard } from "@/features/tasks/task-card";
+import ResetTasksButton from "@/features/tasks/ResetTasksButton";
 
 export default async function Home() {
   const tasks = await fetchTasksAction();
@@ -9,21 +10,24 @@ export default async function Home() {
   const monthlyTasks = tasks.filter((task) => task.frequency === "monthly");
 
   return (
-    <div className=" flex flex-col  w-full h-screen pt-8 items-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-center mb-16">Tasks</h1>
-      <div className="flex gap-20">
-        <section>
-          <h2 className="text-2xl font-bold text-center mb-4">Daily</h2>
-          <TaskCard tasks={dailyTasks} />
-        </section>
-        <section>
-          <h2 className="text-2xl font-bold text-center mb-4">Weekly</h2>
-          <TaskCard tasks={weeklyTasks} />
-        </section>
-        <section>
-          <h2 className="text-2xl font-bold text-center mb-4">Monthly</h2>
-          <TaskCard tasks={monthlyTasks} />
-        </section>
+    <div className="flex flex-col w-full h-screen bg-gray-100">
+      <ResetTasksButton />
+      <h1 className="text-3xl font-bold text-center mt-1 mb-4">Tasks</h1>
+      <div className="flex-1 overflow-y-auto w-full px-4">
+        <div className="flex gap-10 justify-center pb-16">
+          <section>
+            <h2 className="text-2xl font-bold text-center mb-4">Daily</h2>
+            <TaskCard tasks={dailyTasks} />
+          </section>
+          <section>
+            <h2 className="text-2xl font-bold text-center mb-4">Weekly</h2>
+            <TaskCard tasks={weeklyTasks} />
+          </section>
+          <section>
+            <h2 className="text-2xl font-bold text-center mb-4">Monthly</h2>
+            <TaskCard tasks={monthlyTasks} />
+          </section>
+        </div>
       </div>
     </div>
   );
