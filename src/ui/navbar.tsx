4b097184bd7099d0bd849545cfac12d63b6bd6d0
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-28 14:21:22 +0100

    simplified navbar

--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -8,16 +8,14 @@ const navItems = [
 
 export function Navbar() {
   return (
-    <div className="navbar bg-base-100 shadow-md">
-      <ul className="menu menu-horizontal px-1">
-        {navItems.map((item) => (
-          <li key={item.href}>
-            <Link href={item.href} className="btn btn-ghost">
-              {item.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className="menu menu-vertical rounded-box">
+      {navItems.map((item) => (
+        <li key={item.href}>
+          <Link href={item.href} className="btn btn-ghost">
+            {item.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 }
