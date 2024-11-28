cbd57ebcd063d44330bb38a6a97637da51250cd1
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-28 14:13:26 +0100

    revalidate path after deleting or completing

--- src/features/tasks/action.ts
+++ src/features/tasks/action.ts
@@ -1,6 +1,7 @@
 "use server";
 import { TaskInsert } from "@/db/types";
 import { featureService } from "./instance";
+import { revalidatePath } from "next/cache";
 
 export async function postTaskAction(data: FormData) {
   const task = data.get("task") as string;
@@ -15,6 +16,8 @@ export async function postTaskAction(data: FormData) {
 }
 
 export async function completeTaskAction(data: FormData, id: number) {
+  revalidatePath("/");
+
   const completer = data.get("completer") as string;
   await featureService.completeTask(id, completer);
 }
@@ -24,5 +27,6 @@ export async function fetchTasksAction() {
 }
 
 export async function deleteTaskAction(id: number) {
+  revalidatePath("/");
   await featureService.deleteTask(id);
 }
