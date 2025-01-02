0a7ce02e74e55697ae5367f64e7f54d2b73a3723
Daniel Hormos <daniel_danne32@hotmail.com>
2025-01-02 14:34:32 +0100

    created specific button for each reset of tasks

---
+++ src/features/tasks/ResetDailyTasksButton.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { resetDailyTasksAction } from "@/features/tasks/action";
+import { FaSync } from "react-icons/fa";
+
+export default function ResetDailyTasksButton() {
+  return (
+    <button
+      onClick={() => {
+        resetDailyTasksAction();
+      }}
+      className="flex items-center justify-center p-2 bg-gray-100 hover:bg-gray-200 active:bg-gray-300 rounded-full transition-all duration-300 ease-in-out transform hover:scale-110 active:scale-95"
+    >
+      <FaSync
+        size={16}
+        color="gray"
+        className="hover:text-black transition-colors duration-300"
+      />
+    </button>
+  );
+}
